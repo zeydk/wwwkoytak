e10579c973ade7aedc682aec729a151158ed9ea2
zeydk <huzeyd@gmail.com>
2023-09-28 16:42:11 -0400

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,7 @@
   <section class="home">
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
-        <div v-html="$md.render(welcomeText)" class="home__welcome markdown" align="justify"/>
+        <div v-html="$md.render(welcomeText)" class="home__welcome markdown"/>
 
       </div>
       <div class="flex flex-col w-full xl:w-2/5">
