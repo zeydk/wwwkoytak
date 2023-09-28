4b43447ebf2b3c33d36754390aad95e6b1640f46
zeydk <huzeyd@gmail.com>
2023-09-28 15:07:23 -0400

    Update index.vue

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,10 +1,11 @@
+<title> Me, Huseyin Zeyd Koytak </title>
+
 <template>
   <section class="home">
     <div class="py-24 md:py-36 mx-auto flex flex-wrap flex-col md:flex-row items-center">
       <div class="flex flex-col w-full xl:w-3/5 justify-center lg:items-start overflow-y-hidden">
         <div v-html="$md.render(welcomeText)" class="home__welcome markdown" />
 
-   <title> Huseyin Zeyd Koytak </title>
       </div>
       <div class="flex flex-col w-full xl:w-2/5">
         <img
@@ -37,39 +38,6 @@ export default class Home extends Vue {
 
   isSignedUp = false;
 
-  form = {
-    email: '',
-  };
 
-  encode(data): string {
-    return Object.keys(data)
-      .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`)
-      .join('&');
-  }
-
-  validEmail(email): boolean {
-    // eslint-disable-next-line
-    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(email);
-  }
-
-  async handleSubmit(): Promise<void> {
-    if (!this.validEmail(this.form.email)) {
-      this.$refs.emailInput.focus();
-      return;
-    }
-
-    try {
-      await fetch('/', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-        body: this.encode({ 'form-name': 'signups', ...this.form }),
-      });
-
-      this.isSignedUp = true;
-    } catch (error) {
-      console.error(error);
-    }
-  }
 }
 </script>
